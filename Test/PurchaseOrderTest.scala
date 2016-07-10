5072327d9d6bcbc80d24febc5658cbcd1ac03a0d
Luke Smith <lukesmudgesmith@gmail.com>
2016-07-10 19:02:37 +0100

    Completed testing for Product and added skeleton files for further testing

---
+++ Test/PurchaseOrderTest.scala
@@ -0,0 +1,10 @@
+import com.WOTS.system._
+import org.scalatest.{FlatSpec, Matchers, Tag}
+/**
+  * Created by Luke on 10/07/2016.
+  */
+class PurchaseOrderTest extends FlatSpec with Matchers {
+
+}
+
+object PurchaseOrderTestSuccess1 extends Tag("PurchaseOrderTestSuccess1")
